cf133f70bebd37f7e2c115c561d94e5808ad1c57
Wouter Van der Auwera <QCP084@fluvius.be>
2023-09-11 13:52:18 +0200

    repository

---
+++ Xpirit.Chatbot.Domain/Entities/ParkingReservation.cs
@@ -0,0 +1,10 @@
+﻿namespace Xpirit.Chatbot.Domain.Entities
+{
+	public class ParkingReservation
+	{
+		public Guid Id { get; protected set; }
+		public string ParkingSpot { get; set; }
+		public DateTime ReservedDate { get; set; }
+		public string PersonName { get; set; }
+	}
+}
