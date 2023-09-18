8dea2e4be871b502f779230e12d95af7adaf531f
Bruno Van Thournout <bruno.van.thournout@reynaers.com>
2023-09-18 11:03:13 +0200

    remove reservation

---
+++ Xpirit.Chatbot.Domain/Entities/ParkingSpot.cs
@@ -0,0 +1,8 @@
+﻿namespace Xpirit.Chatbot.Domain.Entities
+{
+    public class ParkingSpot
+    {
+        public Guid Id { get; protected set; }
+        public string Name { get; set; } = null!;
+    }
+}
--- Xpirit.Chatbot.Web/Controllers/Parking/ParkingController.cs
+++ Xpirit.Chatbot.Web/Controllers/Parking/ParkingController.cs
@@ -7,79 +7,116 @@ using Xpirit.Chatbot.Domain.Entities;
 [Route("[controller]")]
 public class ParkingController : ControllerBase
 {
-	private readonly IParkingRepository _parkingRepository;
-	private static List<string> _parkingSpots = new() { "Parking 42", "Parking 43", "Parking 44" };
+    private readonly IParkingRepository _parkingRepository;
+    //private static List<string> _parkingSpots = new() { "Parking 42", "Parking 43", "Parking 44" };
 
-	public ParkingController(IParkingRepository parkingRepository)
-	{
-		_parkingRepository = parkingRepository;
-	}
+    public ParkingController(IParkingRepository parkingRepository)
+    {
+        _parkingRepository = parkingRepository;
+    }
 
-	[HttpGet]
-	public ActionResult<IEnumerable<string>> GetParkingSpots()
-	{
-		return Ok(_parkingSpots);
-	}
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<string>>> GetParkingSpots()
+    {
+        var parkingSpots = await _parkingRepository.GetAllParkingSpotsAsync();
 
-	[HttpGet("Reservations")]
-	public async Task<ActionResult<IEnumerable<ParkingReservation>>> GetAllReservations()
-	{
-		var reservations = await _parkingRepository.GetAllReservationsAsync();
-		return Ok(reservations);
-	}
+        return Ok(parkingSpots?.Select(x => x.Name)?.ToList() ?? new List<string>());
+    }
 
-	[HttpGet("Reservations/Available")]
-	public async Task<ActionResult<IEnumerable<string>>> GetAvailableParkingSpotsForDate(DateTime? requestedDate)
-	{
-		if (!requestedDate.HasValue)
-		{
-			return BadRequest($"{nameof(requestedDate)} is not filled in");
-		}
+    [HttpGet("Reservations")]
+    public async Task<ActionResult<IEnumerable<ParkingReservation>>> GetAllReservations(string? personName)
+    {
+        if (string.IsNullOrEmpty(personName))
+        {
+            var reservations = await _parkingRepository.GetAllReservationsAsync();
+            return Ok(reservations);
+        }
+        else
+        {
+            var reservations = await _parkingRepository.GetAllFutureReservationsForPerson(personName);
+            return Ok(reservations);
+        }
+    }
 
-		var reservationsForDate = await _parkingRepository.GetReservationsByDateAsync(requestedDate.Value);
-		var alreadyReservedParkingSpots = reservationsForDate.Select(r => r.ParkingSpot).ToList();
-		var freeParkingSpots = _parkingSpots.Where(x => !alreadyReservedParkingSpots.Contains(x)).ToList();
+    [HttpGet("Reservations/Available")]
+    public async Task<ActionResult<IEnumerable<string>>> GetAvailableParkingSpotsForDate(DateTime? requestedDate)
+    {
+        if (!requestedDate.HasValue)
+        {
+            return BadRequest($"{nameof(requestedDate)} is not filled in");
+        }
 
-		return Ok(freeParkingSpots);
-	}
+        var reservationsForDate = await _parkingRepository.GetReservationsByDateAsync(requestedDate.Value);
+        var alreadyReservedParkingSpots = reservationsForDate.Select(r => r.ParkingSpot).ToList();
 
-	[HttpPost("Reservation")]
-	public async Task<ActionResult<string>> MakeReservation(string? requestedDate, string personName)
-	{
-		if (string.IsNullOrEmpty(requestedDate))
-		{
-			return BadRequest($"{nameof(requestedDate)} is not filled in");
-		}
+        var parkingSpots = await _parkingRepository.GetAllParkingSpotsAsync();
 
-		if (!DateTime.TryParseExact(requestedDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate))
-		{
-			return BadRequest($"{nameof(requestedDate)} is not a valid date");
-		}
+        var freeParkingSpots = parkingSpots.Where(x => !alreadyReservedParkingSpots.Contains(x.Name)).ToList();
 
-		if (string.IsNullOrEmpty(personName))
-		{
-			return BadRequest($"{nameof(personName)} is not filled in");
-		}
+        return Ok(freeParkingSpots);
+    }
 
-		var reservationsForDate = await _parkingRepository.GetReservationsByDateAsync(parsedDate);
-		var alreadyReservedParkingSpots = reservationsForDate.Select(r => r.ParkingSpot).ToList();
-		var freeParkingSpot = _parkingSpots.FirstOrDefault(x => !alreadyReservedParkingSpots.Contains(x));
+    [HttpPost("Reservation")]
+    public async Task<ActionResult<string>> MakeReservation(string? requestedDate, string personName)
+    {
+        if (string.IsNullOrEmpty(requestedDate))
+        {
+            return BadRequest($"{nameof(requestedDate)} is not filled in");
+        }
 
-		if (string.IsNullOrEmpty(freeParkingSpot))
-		{
-			return BadRequest($"No parking spots free for date {requestedDate}");
-		}
-		else
-		{
-			var newReservation = new ParkingReservation
-			{
-				ParkingSpot = freeParkingSpot,
-				ReservedDate = parsedDate,
-				PersonName = personName
-			};
+        if (!DateTime.TryParseExact(requestedDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate))
+        {
+            return BadRequest($"{nameof(requestedDate)} is not a valid date");
+        }
 
-			await _parkingRepository.InsertReservationAsync(newReservation);
-			return Ok(freeParkingSpot);
-		}
-	}
+        if (string.IsNullOrEmpty(personName))
+        {
+            return BadRequest($"{nameof(personName)} is not filled in");
+        }
+
+        var reservationsForDate = await _parkingRepository.GetReservationsByDateAsync(parsedDate);
+        var alreadyReservedParkingSpots = reservationsForDate.Select(r => r.ParkingSpot).ToList();
+
+        var parkingSpots = await _parkingRepository.GetAllParkingSpotsAsync();
+        var freeParkingSpot = parkingSpots.FirstOrDefault(x => !alreadyReservedParkingSpots.Contains(x.Name));
+
+        if (string.IsNullOrEmpty(freeParkingSpot?.Name))
+        {
+            return BadRequest($"No parking spots free for date {requestedDate}");
+        }
+        else
+        {
+            var newReservation = new ParkingReservation
+            {
+                ParkingSpot = freeParkingSpot.Name,
+                ReservedDate = parsedDate,
+                PersonName = personName
+            };
+
+            await _parkingRepository.InsertReservationAsync(newReservation);
+            return Ok(freeParkingSpot);
+        }
+    }
+
+    [HttpDelete("Reservation")]
+    public async Task<ActionResult<string>> RemoveReservation(string? requestedDate, string personName)
+    {
+        if (string.IsNullOrEmpty(requestedDate))
+        {
+            return BadRequest($"{nameof(requestedDate)} is not filled in");
+        }
+
+        if (!DateTime.TryParseExact(requestedDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate))
+        {
+            return BadRequest($"{nameof(requestedDate)} is not a valid date");
+        }
+
+        if (string.IsNullOrEmpty(personName))
+        {
+            return BadRequest($"{nameof(personName)} is not filled in");
+        }
+
+        await _parkingRepository.RemoveReservationAsync(parsedDate, personName);
+        return Ok();
+    }
 }
