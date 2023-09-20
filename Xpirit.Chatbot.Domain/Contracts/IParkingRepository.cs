5acbdf41d662b5c1662c83384abe681dea1d879b
Wouter <woeterman_94@hotmail.com>
2023-09-20 19:54:05 +0200

    Revert "Revert "remove reservation""
    This reverts commit 45492fa94c616730ddf4aeca8b11c1f743ec1b59.

--- Xpirit.Chatbot.Domain/Contracts/IParkingRepository.cs
+++ Xpirit.Chatbot.Domain/Contracts/IParkingRepository.cs
@@ -3,10 +3,13 @@ namespace Xpirit.Chatbot.Domain.Contracts
 {
 	public interface IParkingRepository
 	{
-		Task<List<ParkingReservation>> GetAllReservationsAsync();
-		Task<ParkingReservation> GetByIdAsync(Guid id);
-		Task<List<ParkingReservation>> GetReservationsByDateAsync(DateTime date);
-		Task InsertReservationAsync(ParkingReservation reservation);
+		Task<IEnumerable<ParkingSpot>> GetAllParkingSpotsAsync();
 
-	}
+        Task<IEnumerable<ParkingReservation>> GetAllReservationsAsync();
+		Task<IEnumerable<ParkingReservation>> GetAllFutureReservationsForPerson(string personName);
+        Task<ParkingReservation> GetByIdAsync(Guid id);
+		Task<IEnumerable<ParkingReservation>> GetReservationsByDateAsync(DateTime date);
+		Task InsertReservationAsync(ParkingReservation reservation);
+		Task RemoveReservationAsync(DateTime date, string personName);
+    }
 }
